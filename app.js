32c3ef6d8d04c3ee8e0785698df943d61895f61b
edsonfreitas <edsonfreitasarruda872@gmail.com>
2022-10-21 15:01:40 -0300

    feat: Application middleware configuration

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const admin = require('./routes/admin')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 const path = require('path')
 const app = express()
@@ -10,6 +12,19 @@ const PORT = 8989
 const mongoose = require('mongoose')
 
 //Configurações
+    //Sessão
+    app.use(session({
+        secret: "estaSeraChaveSecreta",
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(flash())
+    //Middleware
+    app.use((req, res, next)=>{
+        res.locals.success_msg = req.flash("success_msg")
+        res.locals.error_msg = req.flash("error_msg")
+        next();
+    })
     //Body Parser
     app.use(express.urlencoded({extended:true}))
     app.use(express.json())
@@ -25,6 +40,7 @@ const mongoose = require('mongoose')
     })
     //Public
     app.use(express.static(path.join(__dirname,"/public")))
+
 //Rotas
 app.get('/', (req,res)=> {
     res.send("Home do Blog")
